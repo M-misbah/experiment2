2fa075b3e9d9d7505ac7cff49a53d2624b61a495
M-misbah <misbahshaikh@eng.rizvi.edu.in>
2024-10-23 23:38:58 +0530

    Add files via upload

---
+++ exp2task1.c
@@ -0,0 +1,22 @@
+#include<stdio.h> 
+int main( ) 
+{ // Name:Misbah Abdul Samad Shaikh 
+  //UIN:241A060
+  //Branch:AIDS
+  //Roll no:51
+    int n;
+    printf("\t\t***Even Odd Finder\n\n");
+    printf("enter a number:");
+    scanf("%d",&n);
+    if (n%2==0)
+    {
+        printf("%d is even",n);
+    }
+    else 
+    {
+        printf("%d is odd",n);
+    }
+    return 0;
+
+}
+         
